6b6b9bd3b6960d455b498f5db8a05a1c946e3f7e
kennedy kariuki <kennedykariuki739@gmail.com>
2025-08-20 22:50:37 +0300

    Register Item model in Django admin with list filters

--- marketplace_app/admin.py
+++ marketplace_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'location', 'price', 'posted_by', 'posted_on']
+    list_filter = ['category', 'posted_on', 'location']
+    search_fields = ['title', 'description', 'location']
+    ordering = ['-posted_on']
+    readonly_fields = ['posted_on']
