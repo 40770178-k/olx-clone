9f9cf6fa99e8a6957a9faff3966f56ef7de7b43e
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-09-02 14:23:12 +0300

    Create UserProfileView to display items for a specific user

--- marketplace_app/views.py
+++ marketplace_app/views.py
@@ -6,6 +6,8 @@ from .forms import ItemForm, UserRegistrationForm
 from .models import Item
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -47,3 +49,20 @@ class ItemListView(ListView):
     model = Item
     template_name = 'item_list.html'
     ordering = ['-posted_on']
+
+class UserprofileView(ListView):
+    model = Item
+    template_name = 'user_profile.html'
+    context_object_name = 'items'
+    paginate_by = 10  # optional, if you want pagination later
+
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        user = get_object_or_404(User, username=username)
+        return Item.objects.filter(user=user).order_by('-posted_on')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.kwargs.get('username')
+        context['profile_user'] = get_object_or_404(User, username=username)
+        return context
