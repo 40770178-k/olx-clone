656a2806183e2c2f1a755ad9aaa8d48a961144ec
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-09-06 15:55:15 +0300

    fixed profile signals

--- marketplace_app/apps.py
+++ marketplace_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MarketplaceAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketplace_app'
+
+    def ready(self):
+        import marketplace_app.signals
