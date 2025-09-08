733f97980a7ce85652fbd1b9a2437a4dbada3a28
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-09-08 16:35:27 +0300

    updated the folder fo cnversation

--- marketplace_project/asgi.py
+++ marketplace_project/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import marketplace_app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace_project.settings')
 
-application = get_asgi_application()
+# Define ASGI application that supports HTTP and WebSocket
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            marketplace_app.routing.websocket_urlpatterns
+        )
+    ),
+})
