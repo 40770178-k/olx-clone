349adb19752ca2e1b2bfaca627dda8a2576877e6
40770178-k <153083964+40770178-k@users.noreply.github.com>
2025-09-10 11:39:17 +0300

    allowed admin to see conversations

--- marketplace_app/admin.py
+++ marketplace_app/admin.py
@@ -1,5 +1,7 @@
+
+
 from django.contrib import admin
-from .models import Item
+from .models import Item, Conversation, Message
 
 # Register your models here.
 @admin.register(Item)
@@ -9,3 +11,23 @@ class ItemAdmin(admin.ModelAdmin):
     search_fields = ['title', 'description', 'location']
     ordering = ['-posted_on']
     readonly_fields = ['posted_on']
+
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'item', 'buyer', 'seller', 'last_message_at', 'created_at']
+    list_filter = ['created_at', 'last_message_at']
+    search_fields = ['item__title', 'buyer__username', 'seller__username']
+    ordering = ['-last_message_at', '-created_at']
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'conversation', 'sender', 'short_content', 'created_at', 'read']
+    list_filter = ['created_at', 'read']
+    search_fields = ['conversation__id', 'sender__username', 'content']
+    ordering = ['-created_at']
+
+    def short_content(self, obj):
+        return (obj.content[:60] + '…') if len(obj.content) > 60 else obj.content
+    short_content.short_description = 'content'
