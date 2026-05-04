5501bf6864e2f0f91e865eaa963c48d0839d0d4f
40770178-k <153083964+40770178-k@users.noreply.github.com>
2026-05-04 07:09:46 +0300

    final touches

--- marketplace_app/admin.py
+++ marketplace_app/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin  # pyright: ignore[reportMissingImports]
 from .models import (
     Item,
     Conversation,
--- marketplace_app/models.py
+++ marketplace_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from django.utils import timezone
+from django.utils import timezone  # pyright: ignore[reportMissingImports]
 
 
 class Item(models.Model):
